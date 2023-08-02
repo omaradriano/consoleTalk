0a6f15d0e143e1f8643fef309f09f85aa0452d29
Omar Adrian <oadrian38@gmail.com>
2023-08-02 17:59:05 -0600

    Se ha integrado una validacion de contraseñas

--- client.js
+++ client.js
@@ -5,6 +5,15 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 }) //Interfaz creada para leer datos por consola
+import colors from 'colors/safe.js'
+import { v4 as uuid_v4 } from 'uuid'
+console.log(uuid_v4());
+
+/**
+ *  green: se ha unido
+ *  magenta: mensaje
+ *  red: ha salido
+ */
 
 //Importancion de la base de datos
 import pool from './DB/dbconn.js'
@@ -12,17 +21,17 @@ import pool from './DB/dbconn.js'
 /**
  * - Realizar el registro de un usuario
  * - Mejorar el login 
- * - Agregar comandos: mostrar los usuarios activos
+ * - Agregar comandos: mostrar los usuarios activos (A medias, aún no se como visualizarlo)
  * - Agregar y borrar usuarios activos
  */
 
 const socket = new Socket()
 socket.connect({ host: '127.0.0.1', port: 8000 })
 socket.setEncoding('utf-8')
-let aciveState = false
+let activeState = false
 
 rl.on('line', async (text) => {
-    if (aciveState) {
+    if (activeState) {
         if (text === '!exit') {
             socket.write(text)
             socket.end()
@@ -31,37 +40,112 @@ rl.on('line', async (text) => {
             //RECODATORIO: no se debe de escribir despues de cerrar el socket por que nos va a llamar a un error.
         }
     } else if (text === '!login') {
-        try {
+        try { //Bloque trycatch que abarca un error en la conexion a la bdd
             const user = await rl.question('User: ')
             const [checkUser] = await pool.query('select * from player where name = ?', [user])
+            // console.log(checkUser);
             // console.log(checkUser[0] + 'Esto proviene de los datos de usuario en caso de que si exista '); //Imprime los datos del usuario
-            if (!checkUser[0]) {
-                throw new Error('---------- No existe el usuario ----------');
+            try { //este bloque abarca errores bajo las condiciones de usuario inexistente
+                if (checkUser.length === 0) {
+                    activeState = false
+                    throw new Error('No existe el usuario en la base de datos')
+                }
+                if (checkUser[0].user_id.length !== 0) {
+                    const inPass = await rl.question('Ingresar password:')
+                    const [pass] = await pool.query('select password from player where name = ?', [user])
+                    // console.log(typeof pass[0].password);
+                    if (inPass === pass[0].password) {
+                        activeState = true
+                        socket.write(`!!activeUser ${user}`)
+                        console.clear()
+                        console.log('---------- Envia mensaje o escribe \'!exit\' para salir ----------');
+                    } else {
+                        activeState = false
+                        throw new Error('Contraseña incorrecta')
+                    }
+                }
+            } catch (err) {
+                console.clear()
+                console.log(err.message);
+                console.log('Se necesita una accion: !login | !register from client');
             }
-            if (checkUser[0].user_id.length !== 0) {
-                const inPass = await rl.question('Ingresar password:')
-                const [pass] = await pool.query('select password from player where name = ?', [user])
-                // console.log(typeof pass[0].password);
-                if(inPass === pass[0].password){
-                    aciveState = true
-                    socket.write(`!!activeUser ${user}`)
-                    console.clear()
-                    console.log('---------- Envia mensaje o escribe \'!exit\' para salir ----------');
-                }else
-                    throw new Error('Contraseña incorrecta')
-                
-            }
-        } catch (error) {
-            console.log(error.message);
-            console.log('Se necesita una accion: !login | !register from client');
+        } catch (err) {
+            console.clear()
+            console.error(`----- Ha habido un error de la conexion a la base de datos -----`)
         }
         // if()
+    } else if (text === '!register') {
+        // const name = await rl.question('Name: ')
+        // const username = await rl.question('Username: ')
+        let pass = await rl.question('Password: ')
+        const regUserTest = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,20}$/g;
+        const regCapitalLetters = /[A-Z]+/g
+        const regNumbers = /\d+/g
+        let passtester = false
+        //Validador de contraseña
+        if (pass.length > 0) {
+            // let passNumbersTester = pass.split('').some((elem, _, arr) => { //Validar si tiene números
+            //     let item = Number(elem)
+            //     return !Number.isNaN(item) ? true : false
+            // })
+            let passCapitalLettersTester = regCapitalLetters.test(pass)
+            let passNumbersTester = regNumbers.test(pass)
+            try {
+                if (pass.length < 8) {
+                    console.clear()
+                    throw new Error('La contraseña no debe tener menos de 8 caracteres')
+                } else if (pass.length > 20) {
+                    console.clear()
+                    throw new Error('La contraseña no debe tener más de 20 caracteres')
+                }
+                if (!passCapitalLettersTester) {
+                    throw new Error('La contraseña debe contener letras masyúsculas')
+                } else {
+                    if (!passNumbersTester) {
+                        throw new Error('La contraseña debe contener almenos un número')
+
+                    } else {
+                        if (regUserTest.test(pass)) {
+                            // console.log('En este punto si jala');
+                            passtester = true
+                        } else {
+                            throw new Error('La contraseña no es válida')
+                        }
+                    }
+                }
+            } catch (error) {
+                console.clear()
+                console.log(colors.yellow(error.message));
+                console.log('Se necesita una accion: !login | !register from client');
+                return
+            }
+            if (passtester) {
+                const confirmPass = await rl.question(`${colors.green('Confirm password: ')}`)
+                if (pass === confirmPass) {
+                    console.clear()
+                    console.log(colors.green('Registrado :D'));
+                    // console.log('Las contraseñas coinciden');
+                } else {
+                    console.clear()
+                    console.log(colors.red('Las contraseñas no coinciden'));
+                    console.log('Se necesita una accion: !login | !register from client');
+                    return
+                }
+            } else {
+                console.log('Apartado donde no sé que pasa');
+            }
+            // console.log(passTester);
+
+        }
+
     } else {
         console.log('Se necesita una accion: !login | !register from client');
     }
 })
 
 socket.on('data', async (message) => {
+    // const allMessage = message.replace(adminReg, message.match(adminReg) + ' ').split(' ')
+
     console.log(message);
 })
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import { Server } from 'net'
+import colors from 'colors/safe.js'
 
 const activeConnections = new Map()
 
@@ -16,51 +17,59 @@ const listen = (port) => {
 
         const adminReg = /^(!|!!)(?=\w)/g
 
-        const user =  `${socket.remoteAddress}:${socket.remotePort}`
+        const user = `${socket.remoteAddress}:${socket.remotePort}`
 
         socket.on('data', (message) => {
-            const allMessage = message.replace(adminReg, message.match(adminReg)+' ').split(' ')
+            const allMessage = message.replace(adminReg, message.match(adminReg) + ' ').split(' ')
             // console.log(allMessage);
-            if(allMessage[0] === '!!'){
-                if(allMessage[1] === 'activeUser'){
-                    console.log(`${allMessage[2]} se ha unido`);
+            if (allMessage[0] === '!!') {
+                if (allMessage[1] === 'activeUser') {
+                    console.log(`${colors.green(allMessage[2])} se ha unido`);
                     activeConnections.set(socket, allMessage[2])
                     // console.log(activeConnections);
                 }
-            }else if(allMessage[0] === '!'){
-                if(allMessage[1] === 'exit'){
+            } else if (allMessage[0] === '!') {
+                if (allMessage[1] === 'exit') {
                     activeConnections.delete(socket)
                     socket.end()
                     console.log(`${user} ha salido`);
-                }else if(allMessage[1] === 'test'){
-                    console.log('Comando test');
-                }else if(allMessage[1] === 'showUsers'){
-                    for(let user of activeConnections.values()){
-                        console.log(`Name: ${user}`);
+                } else if (allMessage[1] === 'test') {
+                    socket.write('Comando test');
+                } else if (allMessage[1] === 'showUsers') {
+                    for (let user of activeConnections.values()) {
+                        socket.write(`Name: ${user}\n`);
                     }
+                } else if (allMessage[1] === 'important') {
+                    socket.write(allMessage[2])
                 }
-            }else{
-                console.log(`${user} -> ${message}`);
+                else {
+                    socket.write('Comando desconocido');
+                }
+            } else {
+                console.log(`${colors.magenta(user)} -> ${message}`);
                 sendMessages(message, socket)
             }
         })
     })
-    server.on('error', (err)=>{
+    server.on('error', (err) => {
         error(err.message)
         activeConnections.delete(socket)
     })
-    server.on('close', ()=>{
-        console.log(`${socket.remoteAddress} ha salido`);
+    server.on('close', () => {
+        console.log(`${colors.red(socket.remoteAddress)} ha salido`);
     })
 }
 
 
 const sendMessages = (message, originUser) => {
     //Enviar mensaje a todos menos a origin
-    for(let user of activeConnections.keys()){
-        if(originUser !== user){
+    for (let user of activeConnections.keys()) {
+        if (originUser !== user) {
             user.write(`${activeConnections.get(user)} -> ${message}`)
         }
+        // if(originUser === user){
+        //     user.write(`Tu -> ${message}`)
+        // }
     }
 
 }
@@ -68,13 +77,13 @@ const sendMessages = (message, originUser) => {
 //Correr el servidor, esta parte ya no se toca de momento
 const main = () => {
     console.log(process.argv);
-    if(process.argv.length !== 3){
+    if (process.argv.length !== 3) {
         error('Faltan argumentos en la expresion')
-    }else{
+    } else {
         let port = process.argv[2]
-        if(isNaN(port)){ 
+        if (isNaN(port)) {
             error(`${port} no es un puerto correcto`)
-        }else{
+        } else {
             port = Number(port)
             listen(port)
         }
